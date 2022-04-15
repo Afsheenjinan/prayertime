bf281c535c147d6613dd713bed8fbd245ac1733c
Afsheenjinan <afsheenameen@gmail.com>
2022-04-15 10:03:05 +0400

    Update functions.dart

--- lib/functions/functions.dart
+++ lib/functions/functions.dart
@@ -2,7 +2,7 @@
 
 import 'dart:math' as math;
 
-double julian(DateTime dateTime,
+double Julian(DateTime dateTime,
     {int hour = 0, int minute = 0, double timezone = 0}) {
   int year = dateTime.year;
   int month = dateTime.month;
@@ -15,6 +15,7 @@ double julian(DateTime dateTime,
 
   int A = (year / 100).floor();
   int B = 2 - A + (A / 4).floor();
+
   return ((365.25 * (year + 4716)).floor() +
       (30.6001 * (month + 1)).floor() +
       day +
@@ -25,36 +26,37 @@ double julian(DateTime dateTime,
 }
 
 // 2451545 is 12:0:0.00 UT on January 1, 2000 .
-double julianCentury(julianDay) => (julianDay - 2451545) / 36525;
+double JulianCentury(julianDay) => (julianDay - 2451545) / 36525;
 
-double degrees(rad) => rad / math.pi * 180;
-double radians(deg) => deg / 180 * math.pi;
+double Degrees(rad) => rad / math.pi * 180;
+double Radians(deg) => deg / 180 * math.pi;
 
-double sin(x) => math.sin(radians(x));
-double cos(x) => math.cos(radians(x));
-double tan(x) => math.tan(radians(x));
+double Sin(x) => math.sin(Radians(x));
+double Cos(x) => math.cos(Radians(x));
+double Tan(x) => math.tan(Radians(x));
 
-double aSin(x) => degrees(math.asin(x));
-double aCos(x) => degrees(math.acos(x));
-double aTan(x) => degrees(math.atan(x));
+double aSin(x) => Degrees(math.asin(x));
+double aCos(x) => Degrees(math.acos(x));
+double aTan(x) => Degrees(math.atan(x));
 
-double aTan2(x, y) => degrees(math.atan2(x, y));
+double aTan2(x, y) => Degrees(math.atan2(x, y));
 
 Map<String, double> getEquationOfTime(double JC) {
-  double Geom_Mean_Long_Sun =
-      (280.46646 + JC * (36000.76983 + JC * 0.0003032)) %
-          360; //Geometric Mean Longitude of the Sun °
+  var Geom_Mean_Long_Sun = (280.46646 + JC * (36000.76983 + JC * 0.0003032)) %
+      360; //Geometric Mean Longitude of the Sun °
+
   double Geom_Mean_Anom_Sun = 357.52911 +
       JC * (35999.05029 - 0.0001537 * JC); // Mean Anomaly of the Sun °
 //  Eccentricity of the Earth’s Orbit
+
   double Eccent_Earth_Orbit =
       0.016708634 - JC * (0.000042037 + 0.0000001267 * JC);
 
 //    Equation of the Center
   double Sun_Eq_of_Ctr =
-      (sin(Geom_Mean_Anom_Sun) * (1.914602 - JC * (0.004817 + 0.000014 * JC)) +
-          sin(2 * Geom_Mean_Anom_Sun) * (0.019993 - 0.000101 * JC) +
-          sin(3 * Geom_Mean_Anom_Sun) * 0.000289);
+      (Sin(Geom_Mean_Anom_Sun) * (1.914602 - JC * (0.004817 + 0.000014 * JC)) +
+          Sin(2 * Geom_Mean_Anom_Sun) * (0.019993 - 0.000101 * JC) +
+          Sin(3 * Geom_Mean_Anom_Sun) * 0.000289);
 
 // True Longitude and True Anomaly °
   double Sun_True_Long = Geom_Mean_Long_Sun + Sun_Eq_of_Ctr;
@@ -63,10 +65,11 @@ Map<String, double> getEquationOfTime(double JC) {
 // Calculate the Radius Vector (AUs)
   double Sun_Rad_Vector =
       (1.000001018 * (1 - Eccent_Earth_Orbit * Eccent_Earth_Orbit)) /
-          (1 + Eccent_Earth_Orbit * cos(Sun_True_Anom));
+          (1 + Eccent_Earth_Orbit * Cos(Sun_True_Anom));
+
 // The Apparent Longitude of the Sun
   double Sun_App_Long =
-      Sun_True_Long - 0.00569 - 0.00478 * sin(125.04 - 1934.136 * JC);
+      Sun_True_Long - 0.00569 - 0.00478 * Sin(125.04 - 1934.136 * JC);
 
 // Obliquity of the Ecliptic # 23.44°
 // ε = 23°26′21.45″ − 46.815″ T − 0.0006″ T2 + 0.00181″ T3
@@ -79,30 +82,52 @@ Map<String, double> getEquationOfTime(double JC) {
 
 // Oblique correction
   double Obliq_Corr =
-      Mean_Obliq_Ecliptic + 0.00256 * cos(125.04 - 1934.136 * JC);
+      Mean_Obliq_Ecliptic + 0.00256 * Cos(125.04 - 1934.136 * JC);
   double Sun_Rt_Ascen =
-      aTan2(cos(Obliq_Corr) * sin(Sun_App_Long), cos(Sun_App_Long));
+      aTan2(Cos(Obliq_Corr) * Sin(Sun_App_Long), Cos(Sun_App_Long));
 
 // Solar declination
-  double Sun_Declin = aSin(sin(Obliq_Corr) * sin(Sun_App_Long));
+  double Sun_Declin = aSin(Sin(Obliq_Corr) * Sin(Sun_App_Long));
 
-  double gamma = tan(Obliq_Corr / 2) * tan(Obliq_Corr / 2);
+  double gamma = Tan(Obliq_Corr / 2) * Tan(Obliq_Corr / 2);
 
 // Equation of Time (minutes)
 
   double Eq_of_Time = 4 *
-      degrees(gamma * sin(2 * Geom_Mean_Long_Sun) -
-          2 * Eccent_Earth_Orbit * sin(Geom_Mean_Anom_Sun) +
+      Degrees(gamma * Sin(2 * Geom_Mean_Long_Sun) -
+          2 * Eccent_Earth_Orbit * Sin(Geom_Mean_Anom_Sun) +
           4 *
               Eccent_Earth_Orbit *
               gamma *
-              sin(Geom_Mean_Anom_Sun) *
-              cos(2 * Geom_Mean_Long_Sun) -
-          0.5 * gamma * gamma * sin(4 * Geom_Mean_Long_Sun) -
+              Sin(Geom_Mean_Anom_Sun) *
+              Cos(2 * Geom_Mean_Long_Sun) -
+          0.5 * gamma * gamma * Sin(4 * Geom_Mean_Long_Sun) -
           1.25 *
               Eccent_Earth_Orbit *
               Eccent_Earth_Orbit *
-              sin(2 * Geom_Mean_Anom_Sun));
+              Sin(2 * Geom_Mean_Anom_Sun));
+/** 
+  // debugPrint('Geom_Mean_Long_Sun : $Geom_Mean_Long_Sun');
+  // debugPrint('Geom_Mean_Anom_Sun : $Geom_Mean_Anom_Sun');
+  // debugPrint('Eccent_Earth_Orbit : $Eccent_Earth_Orbit');
+  // debugPrint('[Sun_Eq_of_Ctr] : $Sun_Eq_of_Ctr');
+  // debugPrint('Sun_True_Long: $Sun_True_Long');
+  // debugPrint('Sun_True_Anom : $Sun_True_Anom');
+  // debugPrint('Sun_Rad_Vector : $Sun_Rad_Vector');
+  // debugPrint('Sun_App_Long : $Sun_App_Long');
+  // debugPrint('Mean_Obliq_Ecliptic : $Mean_Obliq_Ecliptic');
+  // debugPrint('Obliq_Corr : $Obliq_Corr');
+  // debugPrint('Sun_Rt_Ascen : $Sun_Rt_Ascen');
+  // debugPrint('Sun_Declin : $Sun_Declin');
+  // debugPrint('gamma : $gamma');
+  // debugPrint('Eq_of_Time : $Eq_of_Time');
+  **/
 
-  return {'Equation of Time': Eq_of_Time, 'Sun Declination': Sun_Declin};
+  /// debugPrint('Sun_Rt_Ascen : $Sun_Rt_Ascen');
+  return {
+    'Equation of Time': Eq_of_Time,
+    'Sun Declination': Sun_Declin,
+    'Sun_Rad_Vector': Sun_Rad_Vector,
+    'Sun_Rt_Ascen': Sun_Rt_Ascen
+  };
 }
